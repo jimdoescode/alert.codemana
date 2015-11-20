03acea7e54a8d46ff62a67d3a92764f2a3b910e9
jimdoescode <jimdoescode@gmail.com>
2015-11-19 23:26:22 -0500

    Added klein router

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo '';
+require 'vendor/autoload.php';
+
+$klein = new \Klein\Klein();
+
+$klein->respond(['GET', 'POST'], '/', function (\Klein\Request $request, \Klein\Response $response) {
+
+    $response->code(202);
+    $response->body('Hello Response');
+
+    return $response;
+});
+
+$klein->dispatch();
