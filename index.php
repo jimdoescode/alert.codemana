28601e8bbed10c7876c5dca1906ee0df5479ac95
jimdoescode <jimdoescode@gmail.com>
2015-11-25 00:21:30 -0500

    Add some basic looping

--- index.php
+++ index.php
@@ -12,6 +12,12 @@ $app->match('/', function(\Symfony\Component\HttpFoundation\Request $request) {
 
 $app->post('/', function(\Symfony\Component\HttpFoundation\Request $request) {
 
+    $content = json_decode($request->getContent(), true);
+
+    foreach ($content['commits'] as $commit) {
+
+    }
+
     return new \Symfony\Component\HttpFoundation\Response('Hello Silex', 202);
 
 });
