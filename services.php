b92867464d60e4484ffd5a8f57e2f9659315876b
jimdoescode <jimdoescode@gmail.com>
2015-11-25 01:30:52 -0500

    Added github service

--- services.php
+++ services.php
@@ -8,3 +8,19 @@ $app['emailer.service'] = $app->share(function () use ($app) {
     );
 
 });
+
+$app['github.service'] = $app->share(function () use ($app) {
+
+    $client = new \GuzzleHttp\Client([
+        // Default parameters
+        'defaults' => ['debug' => false, 'exceptions' => false],
+        // Base URI is used with relative requests
+        'base_uri' => 'https://api.github.com',
+        // You can set any number of default request options.
+        'timeout'  => 2.0,
+        //TODO: Here we would put in our github oauth credentials
+    ]);
+
+    return new \Alerts\Services\GitHub($client);
+
+});
--- src/Alerts/Services/GitHub.php
+++ src/Alerts/Services/GitHub.php
@@ -3,4 +3,38 @@
 class GitHub
 {
 
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    private $client;
+
+    /**
+     * GitHub constructor.
+     * @param \GuzzleHttp\Client $client
+     */
+    public function __construct(\GuzzleHttp\Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @param string $owner
+     * @param string $repo
+     * @param string $base
+     * @param string $head
+     * @param array $statusFilter
+     * @return array
+     */
+    public function filesChangedInPush($owner, $repo, $base, $head, $statusFilter = [])
+    {
+        $response = $this->client->get("/repos/{$owner}/{$repo}/{$base}...{$head}");
+        if ($response->getStatusCode() >= 200 && $response->getStatusCode() < 300) {
+            $comparison = $response->json();
+
+            return array_filter($comparison['files'], function ($file) use ($statusFilter) {
+                return in_array($file['status'], $statusFilter);
+            });
+        }
+        return [];
+    }
 }
