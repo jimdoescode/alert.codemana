af907a336f5dfbb4cd6b098b9f543ab0b22fd947
jimdoescode <jimdoescode@gmail.com>
2015-11-25 00:22:07 -0500

    Made explicit get route

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 require 'bootstrap.php';
 require 'services.php';
 
-$app->match('/', function(\Symfony\Component\HttpFoundation\Request $request) {
+$app->get('/', function(\Symfony\Component\HttpFoundation\Request $request) {
 
     return new \Symfony\Component\HttpFoundation\Response('Hello Silex', 202);
 
-})->method('GET|POST');
+});
 
 
 $app->post('/', function(\Symfony\Component\HttpFoundation\Request $request) {
