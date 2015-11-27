a5460b164f84154a29f8f0bf67eb2c211bb5b4b9
jimdoescode <jimdoescode@gmail.com>
2015-11-27 11:31:14 -0500

    Add better table container.

--- src/Alerts/Views/htmlEmail.php
+++ src/Alerts/Views/htmlEmail.php
@@ -5,45 +5,47 @@
     </head>
     <body>
 <?php foreach ($patchFiles as $file): ?>
-        <table style="width:100%;border-collapse:collapse;overflow:auto;border:1px solid #ddd;margin-bottom:30px;">
-            <thead>
-                <tr>
-                    <td colspan="3" style="padding:5px 10px;background-color:#f7f7f7;border-bottom:1px solid #d8d8d8;">
-                        <div style="color:#999;float:right;line-height:32px;font-size:12px;font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;font-style:italic;">
-                            Editor<?=count($file->editors) > 1 ? 's' : '';?>: <?=implode(',&nbsp;', $file->editors);?>
-                        </div>
-                        <div style="color:#999;float:left;line-height:32px;font-size:12px;font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;">
-                            <?=$file->name;?>
-                        </div>
-                    </td>
-                </tr>
-            </thead>
-            <tbody>
+        <div style="width:100%;overflow-x:scroll;border:1px solid #ddd;margin-bottom:30px;">
+            <table style="border-collapse:collapse;">
+                <thead>
+                    <tr>
+                        <td colspan="3" style="padding:5px 10px;background-color:#f7f7f7;border-bottom:1px solid #d8d8d8;">
+                            <div style="color:#999;float:right;line-height:32px;font-size:12px;font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;font-style:italic;">
+                                Editor<?=count($file->editors) > 1 ? 's' : '';?>: <?=implode(',&nbsp;', $file->editors);?>
+                            </div>
+                            <div style="color:#999;float:left;line-height:32px;font-size:12px;font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;">
+                                <?=$file->name;?>
+                            </div>
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>
     <?php foreach ($file->chunks as $chunk): ?>
-                <tr style="background-color:#eee;color:#999;">
-                    <td colspan="2" style="font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:12px;line-height:18px;vertical-align:top;text-align: center;padding-left:10px;padding-right:10px;border-right:1px solid #ddd;">
-                        ...
-                    </td>
-                    <td style="font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:12px;line-height:18px;vertical-align:top;padding-left:10px;">
-                        <?=$chunk->header;?>
-                    </td>
-                </tr>
+                    <tr style="background-color:#eee;color:#999;">
+                        <td colspan="2" style="font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:12px;line-height:18px;vertical-align:top;text-align: center;padding-left:10px;padding-right:10px;border-right:1px solid #ddd;">
+                            ...
+                        </td>
+                        <td style="font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:12px;line-height:18px;vertical-align:top;padding-left:10px;">
+                            <?=$chunk->header;?>
+                        </td>
+                    </tr>
         <?php foreach ($chunk->lines as $line): ?>
-                <tr style="background-color: <?=$line->isRemoved ? '#f99' : ($line->isAdded ? '#9e9' : '#fff');?>;">
-                    <td style="color:#888;width:1%;min-width:25px;font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:12px;line-height:18px;vertical-align:top;text-align:right;padding-left:10px;padding-right:10px;border-right:1px solid <?=$line->isRemoved ? '#f88' : ($line->isAdded ? '#7d7' : '#eee');?>;">
-                        <?=$line->oldNumber;?>
-                    </td>
-                    <td style="color:#888;width:1%;min-width:25px;font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:12px;line-height:18px;vertical-align:top;text-align:right;padding-left:10px;padding-right:10px;border-right:1px solid <?=$line->isRemoved ? '#f88' : ($line->isAdded ? '#7d7' : '#eee');?>;">
-                        <?=$line->newNumber;?>
-                    </td>
-                    <td style="color:#444;font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:12px;line-height:18px;vertical-align:top;padding-left:10px;">
-                        <pre style="margin:0;white-space:pre !important;"><code style="white-space:pre !important;"><?=$line->parsed;?></code></pre>
-                    </td>
-                </tr>
+                    <tr style="background-color: <?=$line->isRemoved ? '#f99' : ($line->isAdded ? '#9e9' : '#fff');?>;">
+                        <td style="color:#888;width:1%;min-width:25px;font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:12px;line-height:18px;vertical-align:top;text-align:right;padding-left:10px;padding-right:10px;border-right:1px solid <?=$line->isRemoved ? '#f88' : ($line->isAdded ? '#7d7' : '#eee');?>;">
+                            <?=$line->oldNumber;?>
+                        </td>
+                        <td style="color:#888;width:1%;min-width:25px;font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:12px;line-height:18px;vertical-align:top;text-align:right;padding-left:10px;padding-right:10px;border-right:1px solid <?=$line->isRemoved ? '#f88' : ($line->isAdded ? '#7d7' : '#eee');?>;">
+                            <?=$line->newNumber;?>
+                        </td>
+                        <td style="color:#444;font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:12px;line-height:18px;vertical-align:top;padding-left:10px;">
+                            <pre style="margin:0;white-space:pre !important;"><code style="white-space:pre !important;"><?=$line->parsed;?></code></pre>
+                        </td>
+                    </tr>
         <?php endforeach; ?>
     <?php endforeach; ?>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
 <?php endforeach; ?>
     </body>
 </html>
