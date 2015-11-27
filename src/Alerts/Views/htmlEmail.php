81f9ea8502fc6a4a1a6df63bd345c17616929c6d
jimdoescode <jimdoescode@gmail.com>
2015-11-27 16:18:03 -0500

    More style fixes

--- src/Alerts/Views/htmlEmail.php
+++ src/Alerts/Views/htmlEmail.php
@@ -5,8 +5,8 @@
     </head>
     <body>
 <?php foreach ($patchFiles as $file): ?>
-        <div style="width:100%;overflow-x:scroll;border:1px solid #ddd;margin-bottom:30px;">
-            <table style="border-collapse:collapse;">
+        <div style="width:99%;overflow-x:auto;border:1px solid #ddd;margin-bottom:30px;">
+            <table style="border-collapse:collapse;overflow:visible;">
                 <thead>
                     <tr>
                         <td colspan="3" style="padding:5px 10px;background-color:#f7f7f7;border-bottom:1px solid #d8d8d8;">
@@ -30,7 +30,7 @@
                         </td>
                     </tr>
         <?php foreach ($chunk->lines as $line): ?>
-                    <tr style="background-color: <?=$line->isRemoved ? '#f99' : ($line->isAdded ? '#9e9' : '#fff');?>;">
+                    <tr style="background-color:<?=$line->isRemoved ? '#f99' : ($line->isAdded ? '#9e9' : '#fff');?>;">
                         <td style="color:#888;width:1%;min-width:25px;font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:12px;line-height:18px;vertical-align:top;text-align:right;padding-left:10px;padding-right:10px;border-right:1px solid <?=$line->isRemoved ? '#f88' : ($line->isAdded ? '#7d7' : '#eee');?>;">
                             <?=$line->oldNumber;?>
                         </td>
@@ -38,7 +38,7 @@
                             <?=$line->newNumber;?>
                         </td>
                         <td style="color:#444;font-family:Consolas,'Liberation Mono',Menlo,Courier,monospace;font-size:12px;line-height:18px;vertical-align:top;padding-left:10px;">
-                            <pre style="margin:0;white-space:pre !important;"><code style="white-space:pre !important;"><?=$line->parsed;?></code></pre>
+                            <pre style="margin:0;white-space:pre !important;overflow:visible;"><code style="white-space:pre !important;"><?=$line->parsed;?></code></pre>
                         </td>
                     </tr>
         <?php endforeach; ?>
