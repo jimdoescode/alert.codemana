38907de6cc69b25974e05b9305f4c0b81bd25fa8
jimdoescode <jimdoescode@gmail.com>
2016-01-29 00:50:40 -0500

    Add new fields but don't show the actual values.

--- src/Alerts/Config/dev.php
+++ src/Alerts/Config/dev.php
@@ -3,21 +3,34 @@
 return [
 
     'project.name' => 'CodeMana Alerts',
+    'project.url' => 'https://alerts.codemana.com',
 
     'debug' => true,
 
     'email' => [
-        'from' => 'test@alerts.codemana.edu',
+        'from' => 'test@alerts.codemana.com',
         'name' => 'CodeMana Alert'
+    ],
+
+    'github' => [
+        'client_id' => '',
+        'client_secret' => ''
+    ],
+
+    'log' => [
+        'level' => \Monolog\Logger::DEBUG,
+        'name' => 'codemana.alerts',
+        'key' => 'logs',
+        'file' => __DIR__.'/../../../errors.monolog.log'
     ],
 
     'swiftmailer.use_spool' => false,
     'swiftmailer.options' => [
-        'host' => 'localhost',
-        'port' => '25',
+        'host' => '',
+        'port' => '587',
         'username' => '',
         'password' => '',
-        'encryption' => null,
+        'encryption' => 'tls',
         'auth_mode' => null,
     ],
 
