36200f085c0ae79514d65731bc9603827e322eac
jimdoescode <jimdoescode@gmail.com>
2016-02-16 21:15:43 -0500

    GitHub login functionality is now handled by the client.

--- client/components/App.js
+++ client/components/App.js
@@ -10,16 +10,55 @@ var LoginButtons = require("./LoginButtons.js");
 
 App = React.createClass({
     propTypes: {
-        'isLoggedIn': React.PropTypes.bool,
         'githubClientId': React.PropTypes.string
     },
 
+    getURLParameter: function(name) {
+        return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+    },
+
+    getInitialState: function() {
+        return {
+            "isLoggedIn": false,
+            "githubStateParam": null
+        };
+    },
+
+    attemptGitHubLogin: function() {
+        var code = this.getURLParameter('code');
+        var state = this.getURLParameter('state');
+        if (code === null) {
+            var githubState = Math.random().toString(36).slice(2);
+            this.setState({"githubStateParam": githubState});
+            localStorage.setItem('githubState', githubState);
+        } else if (state !== null){
+            var url = '/github/login';
+            var self = this;
+            this.setState({"githubStateParam": state});
+            window.fetch(url + '?code='+code+'&state='+state).then(function(response) {
+                return response.json()
+            }).then(function(json) {
+                self.setState({
+                    'isLoggedIn': true,
+                    'user': json['user'],
+                    'token': json['token']
+                });
+                console.log(json);
+            });
+        }
+    },
+
+    componentWillMount: function() {
+        this.attemptGitHubLogin();
+    },
+
     render: function() {
+        var loginButtons = <LoginButtons githubClientId={this.props.githubClientId} githubStateParam={this.state.githubStateParam}/>;
         return (
             <div className="app">
                 <AppHeader/>
                 <div className="container main">
-                    {this.props.isLoggedIn ? <Watcher/> : <LoginButtons/>}
+                    {this.state.isLoggedIn ? <Watcher/> : loginButtons}
                 </div>
                 <AppFooter/>
             </div>
--- client/components/LoginButtons.js
+++ client/components/LoginButtons.js
@@ -1,9 +1,31 @@
 var React = require("react");
 
 module.exports = React.createClass({
+    propTypes: {
+        'githubAuthUrl': React.PropTypes.string,
+        'githubScopes': React.PropTypes.string,
+        'githubStateParam': React.PropTypes.string,
+        'githubClientId': React.PropTypes.string,
+
+        'bitbucketAuthUrl': React.PropTypes.string,
+        'bitbucketClientId': React.PropTypes.string
+    },
+
+    getDefaultProps: function() {
+        return {
+            'githubAuthUrl': 'https://github.com/login/oauth/authorize',
+            'githubScopes': 'user:email,repo'
+        };
+    },
 
     render: function() {
-        return <div></div>
+        return (
+            <div>
+                <a href={this.props.githubAuthUrl+"?client_id="+this.props.githubClientId+"&scope="+this.props.githubScopes+"&state="+this.props.githubStateParam}>
+                    GitHub Login
+                </a>
+            </div>
+        );
     }
 
 });
